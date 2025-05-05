7a907638baaaa99fb06427ae997b59b67b2f220a
ayberk cakmak <ayberkcakmak@gmail.com>
2025-05-05 18:45:23 +0300

    data acquired

--- scripts/main.m
+++ scripts/main.m
@@ -11,7 +11,7 @@ params.warnings = {};
 %%%%%%%%%%%% INPUTS
 % maximum dimension (must be an even number)
 dimMin = 2;
-dimMax = 10;
+dimMax = 14;
 
 %%% folder paths 
 params.paths.matricesPath = fullfile(pwd,'..','matrices');
